7cf2d2a77b37b6cfb42a752640ba76acb051ac41
jmoroyoqui <moroyoquigil.julio@gmail.com>
2024-09-29 14:11:42 -0700

    creating database from entityframework core

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
@@ -19,6 +20,13 @@ namespace eTickets.Models
         public List<Actor_Movie> Actors_Movies { get; set; }
 
         //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
         public Cinema Cinema { get; set; }
+
+        // Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
     }
 }
