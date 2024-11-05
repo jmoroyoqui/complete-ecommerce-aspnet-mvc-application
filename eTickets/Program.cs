bf3c2517a25d020e3f938e70b854be48372306de
jmoroyoqui <moroyoquigil.julio@gmail.com>
2024-11-05 10:11:56 -0700

    Producers functionality
    Adding CRUD for producers, including interfaces, services, models, views and controller

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddScoped<IProducersService, ProducersService>();
 
 var app = builder.Build();
 AppDbInitializer.Seed(app);
