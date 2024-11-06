48496546a9747fd7928a258e821ec08629fcc210
jmoroyoqui <moroyoquigil.julio@gmail.com>
2024-11-06 11:36:45 -0700

    Cinemas functionallity
    Added interfaces, services, models, controller and views for CRUD Cinemas

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,19 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
-    public class Cinema
+    public class Cinema:IEntityBase
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name = "Image")]
+        [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage ="Cinema logo is required")]
         public string Logo { get; set; }
         [Display(Name = "Name")]
+        [Required(ErrorMessage = "Cinema name is required")]
         public string Name { get; set; }
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Cinema description is required")]
         public string Description { get; set; }
 
         //Relationship
-        public List<Movie> Movies { get; set; }
+        public List<Movie>? Movies { get; set; }
     }
 }
