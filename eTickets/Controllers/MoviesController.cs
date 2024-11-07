39b31c94cfe1000a5ed0afb36fad7968036c63d3
jmoroyoqui <moroyoquigil.julio@gmail.com>
2024-11-07 14:59:31 -0700

    Changes in Movies Controller and general functionality

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using eTickets.Data.Services;
 using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
@@ -11,12 +12,106 @@ namespace eTickets.Controllers
         private readonly IMoviesService _service;
         public MoviesController(IMoviesService service)
         {
-                _service = service;
+            _service = service;
         }
         public async Task<IActionResult> Index()
         {
             var allMovies = await _service.GetAllAsync(n => n.Cinema);
             return View(allMovies);
+        }
+
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
+                return View("Index", filteredResult);
+            }
+            return View("Index", allMovies);
+        }
+
+        // GET: Movies/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var movieDetail = await _service.GetMovieByIdAsync(id);
+            return View(movieDetail);
+        }
+
+        //GET: Movies/Create
+        public async Task<IActionResult> Create()
+        {
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewMovieVM newMovie)
+        {
+            if (!ModelState.IsValid)
+            {
+                var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+                ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+                ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+                ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+                return View(newMovie);
+            }
+            await _service.AddNewMovieAsync(newMovie);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: Movies/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var movieDetails = await _service.GetMovieByIdAsync(id);
+
+            if (movieDetails == null) return View("NotFound");
+
+            var response = new NewMovieVM()
+            {
+                Id = movieDetails.Id,
+                Name = movieDetails.Name,
+                Description = movieDetails.Description,
+                Price = movieDetails.Price,
+                ImageURL = movieDetails.ImageURL,
+                StartDate = movieDetails.StartDate,
+                EndDate = movieDetails.EndDate,
+                MovieCategory = movieDetails.MovieCategory,
+                CinemaId = movieDetails.CinemaId,
+                ProducerId = movieDetails.ProducerId,
+                ActorIds = movieDetails.Actors_Movies.Select(n => n.ActorId).ToList()
+            };
+
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, NewMovieVM movie)
+        {
+            if (id != movie.Id) return View("NotFound");
+
+            if (!ModelState.IsValid)
+            {
+                var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+                ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+                ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+                ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+                return View(movie);
+            }
+
+            await _service.UpdateMovieAsync(movie);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ eTickets/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,54 @@
+﻿using eTickets.Data.Base;
+using eTickets.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class NewMovieVM
+    {
+        public int Id { get; set; }
+        [Display(Name = "Movie Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Movie Description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Display(Name = "Movie Price in $")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Movie Poster URL")]
+        [Required(ErrorMessage = "Movie Poster URL is required")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Start date")]
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End date")]
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Select a category")]
+        [Required(ErrorMessage = "Movie category is required")]
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationship
+        [Display(Name = "Select actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is required")]
+        public List<int> ActorIds { get; set; }
+
+        //Cinema
+        [Display(Name = "Select a cinema")]
+        [Required(ErrorMessage = "Movie cinema is required")]
+        public int CinemaId { get; set; }
+
+        // Producer
+        [Display(Name = "Select a producer")]
+        [Required(ErrorMessage = "Movie producer is required")]
+        public int ProducerId { get; set; }
+    }
+}
