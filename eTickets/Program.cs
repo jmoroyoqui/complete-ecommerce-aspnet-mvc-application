4dc21702580c7dcfd7d6792891bc3d1ca2a8115b
jmoroyoqui <moroyoquigil.julio@gmail.com>
2024-11-28 09:14:58 -0700

    fixing authorization errors
    changes in program.cs in order to avoid the error for authentication and authorization at the start.

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,6 +1,9 @@
 using eTickets.Data;
 using eTickets.Data.Cart;
 using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +19,17 @@ builder.Services.AddScoped<IMoviesService, MoviesService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<IOrdersService, OrdersService>();
 builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+// Authentication and authorization
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
+
 builder.Services.AddSession();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
 
 var app = builder.Build();
 AppDbInitializer.Seed(app);
@@ -35,6 +48,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseSession();
+
+//Authentication and authorization
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAuthorization();
 
